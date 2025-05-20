c62e4c82e32212030934e8440c598350484d3ca3
VeldiApparao <apparao.arun@gmail.com>
2025-05-20 11:28:01 +0530

    This code is for escape charector code

---
+++ 13_Escape_charector.py
@@ -0,0 +1,16 @@
+print ("My name is Apparao")
+
+#print ("My name is "Apparao"")
+
+print ("My name is \"Apparao\"")
+
+print ("Hellow")
+print("World")
+
+print ("Hello\nWorld")
+
+print("12345678\rHello")
+
+print ("Hello\tWorld")
+
+print ("Hello\bWorld")
